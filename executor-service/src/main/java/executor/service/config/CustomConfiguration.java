de4336ccda3fb771ee019b060237d2de23eab197
OleksandrShevchenko21 <shevchenko2106@gmail.com>
2023-07-12 19:00:49 +0300

    WebDriver migration

--- executor-service/src/main/java/executor/service/config/CustomConfiguration.java
+++ executor-service/src/main/java/executor/service/config/CustomConfiguration.java
@@ -7,10 +7,6 @@ import executor.service.maintenance.plugin.proxy.JsonProxySources;
 import executor.service.maintenance.plugin.proxy.ProxySourcesClient;
 import executor.service.maintenance.plugin.proxy.ProxySourcesClientImpl;
 import executor.service.model.WebDriverConfigDto;
-import executor.service.stepexecution.ClickCss;
-import executor.service.stepexecution.ClickXpath;
-import executor.service.stepexecution.Sleep;
-import executor.service.stepexecution.StepExecution;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,21 +42,6 @@ public class CustomConfiguration {
         return new ChromeDriverProviderImpl(new ProxyProviderImpl(), webDriverConfigDto());
     }
 
-//    @Bean
-//    public ThreadPoolConfigDto threadPoolConfigDto() {
-//        return PropertyReader.threadPoolConfigDtoFromProperties();
-//    }
-
-//    @Bean
-//    public ScenarioExecutor scenarioExecutor() {
-//        Logger scenario_logger = LoggerFactory.getLogger("SCENARIO_LOGGER");
-//        List<StepExecution> steps = List.of(
-//                LoggingProxyProvider.createProxy(new ClickCss(), StepExecution.class, scenario_logger),
-//                LoggingProxyProvider.createProxy(new ClickXpath(), StepExecution.class, scenario_logger),
-//                LoggingProxyProvider.createProxy(new Sleep(), StepExecution.class, scenario_logger)
-//        );
-//        return LoggingProxyProvider.createProxy(new ScenarioExecutorImpl(steps), ScenarioExecutor.class, scenario_logger);
-//    }
     @Bean
     public ProxySourcesClient proxySourcesClient() {
         return new ProxySourcesClientImpl(new JsonProxySources());
